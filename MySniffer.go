1ccddeab65240f81de1912d35d8186a90ba259f4
Tangent_orz <tangent0orz@gmail.com>
2022-03-27 21:52:46 +0800

    增加网卡选择
    增加网卡选择界面

---
+++ MySniffer.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"net"
+)
+
+var INTERFACENAME = "Please choice an interface!"
+
+type Interface struct {
+	Name        string //设备名称
+	Description string //设备描述信息
+	Flags       uint32
+	Addresses   []InterfaceAddress //网口的地址信息列表
+}
+
+// InterfaceAddress describes an address associated with an Interface.
+// Currently, it's IPv4/6 specific.
+type InterfaceAddress struct {
+	IP        net.IP
+	Netmask   net.IPMask // Netmask may be nil if we were unable to retrieve it.
+	Broadaddr net.IP     // Broadcast address for this IP may be nil
+	P2P       net.IP     // P2P destination address for this IP may be nil
+}
+
+var allInterfaceInfo = make(map[string]Interface)
+
+func getInterfaceList() []string {
+	var interfaceList []string
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, d := range devices {
+		interfaceList = append(interfaceList, d.Name)
+		var addressList []InterfaceAddress
+		for _, address := range d.Addresses {
+			addressList = append(addressList, InterfaceAddress{address.IP, address.Netmask, address.Broadaddr, address.P2P})
+		}
+		allInterfaceInfo[d.Name] = Interface{d.Name, d.Description, d.Flags, addressList}
+	}
+	return interfaceList
+}
+
+func getInterfaceDetails(interfaceName string) string {
+	if interfaceInfo, ok := allInterfaceInfo[interfaceName]; ok {
+		var interfaceInfoString string
+		interfaceInfoString += "Description: "
+		interfaceInfoString += interfaceInfo.Description + "\n"
+		for _, address := range interfaceInfo.Addresses {
+			interfaceInfoString += "- IP address: "
+			interfaceInfoString += address.IP.String() + "\n"
+			interfaceInfoString += "- Subnet mask: " + address.Netmask.String() + "\n"
+		}
+		fmt.Println(interfaceInfoString)
+		return interfaceInfoString
+	}
+	return "??"
+}
+
+func setINTERFACENAME(name string) {
+	INTERFACENAME = name
+	fmt.Println("set interface is " + name)
+}
+
+func getINTERFACENAME() string {
+	return INTERFACENAME
+}
--- main.go
+++ main.go
@@ -1,16 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"time"
 )
 
 func main() {
 	myApp := app.NewWithID("UCAS")
 	myWin := myApp.NewWindow("UCAS")
+
+	interfaceCard := widget.NewLabel(getINTERFACENAME())
+
 	myWin.SetMainMenu(makeMenu(myApp, myWin))
+	myWin.SetContent(interfaceCard)
+	go func() {
+		for range time.Tick(time.Second) {
+			interfaceCard.SetText(getINTERFACENAME())
+			fmt.Println(getINTERFACENAME())
+		}
+	}()
 	myWin.Resize(fyne.NewSize(640*2, 640))
+
 	myWin.ShowAndRun()
+}
+
+func makeInterfaceWin(a fyne.App, w fyne.Window) fyne.Window {
+	//实现接口选择窗口
+	var interfacename string
+	label := widget.NewLabel("Select A Interface From The List")
+	hbox := container.NewHBox(label)
+	interfaceNameList := getInterfaceList()
+	interfaceWindow := a.NewWindow("Interface Choice")
+	//设置list
+	interfaceList := widget.NewList(
+		func() int {
+			return len(interfaceNameList)
+		},
+		func() fyne.CanvasObject {
+			return container.NewHBox(widget.NewLabel("interface "))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[0].(*widget.Label).SetText(interfaceNameList[id])
+		})
+	interfaceList.OnSelected = func(id widget.ListItemID) {
+		interfacename = interfaceNameList[id]
+		label.SetText(getInterfaceDetails(interfaceNameList[id]))
+	}
+	interfaceList.OnUnselected = func(id widget.ListItemID) {
+		label.SetText("lect An Item From The List")
+	}
+	interfaceList.Select(125)
+
+	choiceButton := widget.NewButton("confirm", func() {
+		setINTERFACENAME(interfacename)
+		interfaceWindow.Close()
+	})
+	interfaceWindow.SetContent(container.NewHSplit(interfaceList, container.NewVBox(hbox, choiceButton)))
+	interfaceWindow.Resize(fyne.NewSize(640*2, 640))
+	return interfaceWindow
+
 }
 
 func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
@@ -28,7 +80,11 @@ func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
 	editMenu := fyne.NewMenu("Edit", editCopyItem, editPasteItem, editCutItem)
 
 	//定义interface菜单
-	interfaceMenu := fyne.NewMenu("Interface")
+	interfaceMenu := fyne.NewMenuItem("Interface", func() {
+		interfaceWindow := makeInterfaceWin(a, w)
+		interfaceWindow.Show()
+
+	})
 
 	// 定义help菜单
 	helpDocItem := fyne.NewMenuItem("Doc", nil)
@@ -38,7 +94,7 @@ func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
 	return fyne.NewMainMenu(
 		fileMenu,
 		editMenu,
-		interfaceMenu,
+		fyne.NewMenu("interfaceMenu", interfaceMenu),
 		helpMenu,
 	)
 }
