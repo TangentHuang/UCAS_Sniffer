cc56a2adc2ffc54b6158b62fd4c0346c1bb4dacb
Tangent_orz <tangent0orz@gmail.com>
2022-03-30 23:46:18 +0800

    完成基本功能
    完成基本功能

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ## 基础设计
 
 项目主体使用Golang语言编写，GUI使用基于Golang的Fyne框架，网络嗅探部分使用gopacket库
+
+## 界面
+
+![](https://img.tangent.ink/5eb52b12e466e8ffff912f38f0df16e.png)
--- makeMainContent.go
+++ makeMainContent.go
@@ -6,47 +6,61 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"github.com/google/gopacket"
+	"strconv"
 	"time"
 )
 
 type netPacket struct {
 	SrcIP    string
-	DstIP   string
+	DstIP    string
 	Protocol string
 	SrcPort  string
 	DstPort  string
-	Length int
-	packet gopacket.Packet
+	Length   int
+	packet   gopacket.Packet
 }
+
 //packet的长度
 var netPacketLen int
 
-
-
 func makeMainContent() fyne.CanvasObject {
-	head := container.NewGridWithColumns(4, widget.NewLabel("Source"),
+	packetDetails := widget.NewTextGrid()
+	packetDump := widget.NewTextGrid()
+	head := container.NewGridWithColumns(4,
+		widget.NewLabel("Source"),
 		widget.NewLabel("Destination"),
 		widget.NewLabel("Protocol"),
 		widget.NewLabel("Length"))
 	list := widget.NewList(func() int {
 		return netPacketLen
-	} ,func() fyne.CanvasObject {
-		// !todo 获取信息
+	}, func() fyne.CanvasObject {
 		hb := container.NewGridWithColumns(4)
 		for i := 0; i < 4; i++ {
 			hb.Add(widget.NewLabel(fmt.Sprintf("Field %d", i)))
 		}
 		return hb
 	}, func(id widget.ListItemID, object fyne.CanvasObject) {
-
+		hb := object.(*fyne.Container)
+		lbl0 := hb.Objects[0].(*widget.Label)
+		lbl0.SetText(netPacketList[id].SrcIP)
+		lbl1 := hb.Objects[1].(*widget.Label)
+		lbl1.SetText(netPacketList[id].DstIP)
+		lbl2 := hb.Objects[2].(*widget.Label)
+		lbl2.SetText(netPacketList[id].Protocol)
+		lbl3 := hb.Objects[3].(*widget.Label)
+		lbl3.SetText(strconv.Itoa(netPacketList[id].Length))
 	})
+	list.OnSelected = func(id widget.ListItemID) {
+		packetDetails.SetText(netPacketList[id].packet.String())
+		packetDump.SetText(netPacketList[id].packet.Dump())
+	}
 	//定时刷新list
 	go func() {
-		for range time.Tick(2*time.Second){
+		for range time.Tick(2 * time.Second) {
 			list.Refresh()
 		}
 	}()
 
 	listContent := container.NewBorder(head, nil, nil, nil, list)
-	return container.NewVSplit(listContent, container.NewHSplit(widget.NewLabel("11"), widget.NewLabel("22")))
+	return container.NewVSplit(listContent, container.NewVSplit(container.NewScroll(packetDetails), container.NewScroll(packetDump)))
 }
